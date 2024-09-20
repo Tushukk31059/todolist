d4115108366b695220057b3bdf5a5a23699bbb70
Tushukk31059 <138555958+Tushukk31059@users.noreply.github.com>
2024-09-20 23:26:05 +0530

    Update MainActivity.kt

--- app/src/main/java/com/tushar/todolist/MainActivity.kt
+++ app/src/main/java/com/tushar/todolist/MainActivity.kt
@@ -2,10 +2,28 @@ package com.tushar.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.app.AlarmManager
+import android.app.MediaRouteButton
+import android.content.AbstractThreadedSyncAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.TimePicker
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var addButton: Button
+    private lateinit var taskInput: EditText
+    private lateinit var timePicker: TimePicker
+    private lateinit var periodSpinner: Spinner
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        timePicker=findViewById(R.id.timePicker)
+        periodSpinner=findViewById(R.id.periodSpinner)
+        addButton=findViewById(R.id.addButton)
+        taskInput=findViewById(R.id.taskInput)
+
     }
 }
